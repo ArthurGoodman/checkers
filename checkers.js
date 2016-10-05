011573f1936a782c1ce7023e9ea22ccd1a050354
ArthurGoodman <arthur.goodman3@gmail.com>
2016-10-05 23:15:06 +0300

    jquery, bootstrap, websockets

---
+++ checkers.js
@@ -0,0 +1,31 @@
+var websocket = null;
+
+$(document).ready(function initWebSocket() {
+    try {
+        if (typeof MozWebSocket == 'function')
+            WebSocket = MozWebSocket;
+
+        if (websocket && websocket.readyState == 1)
+            websocket.close();
+
+        websocket = new WebSocket("ws://localhost:43567");
+
+        websocket.onopen = function(evt) {};
+
+        websocket.onclose = function(evt) {};
+
+        websocket.onmessage = function(evt) {
+            console.log("message: ", evt.data);
+        };
+
+        websocket.onerror = function(evt) {};
+    } catch (exception) {
+        console.log("error: ", evt.data);
+    }
+});
+
+function sendMessage() {
+    if (websocket != null) {
+        websocket.send("message");
+    }
+}
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char **argv) {
     QCoreApplication app(argc, argv);
 
     Server server;
+    system("checkers.html");
 
     return app.exec();
 }
