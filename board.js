a0911c8bc8f6a457f070553938f6b901f011517f
ArthurGoodman <arthur.goodman3@gmail.com>
2016-10-14 01:21:10 +0300

    drawing optimization

--- board.js
+++ board.js
@@ -15,8 +15,22 @@ function Board(canvas) {
 
     var highlights = [];
 
+    var redraw = true;
+
     function indexAt(x, y) {
         return y * BoardDim + x;
+    }
+
+    function getX(index) {
+        return index % BoardDim;
+    }
+
+    function getY(index) {
+        return Math.floor(index / BoardDim);
+    }
+
+    function at(x, y) {
+        return data[indexAt(x, y)];
     }
 
     this.resize = function(size) {
@@ -26,6 +40,8 @@ function Board(canvas) {
         canvas.height = boardSize;
 
         cellSize = boardSize / BoardDim;
+
+        redraw = true;
     }
 
     this.isAnimating = function() {
@@ -41,6 +57,7 @@ function Board(canvas) {
         selection = from;
         target = to;
         animation = 0;
+        redraw = true;
     }
 
     this.map = function(x, y) {
@@ -49,133 +66,131 @@ function Board(canvas) {
 
     this.setData = function(d) {
         data = d;
+        redraw = true;
     }
 
     this.select = function(index) {
         selection = index;
+        redraw = true;
     }
 
     this.deselect = function() {
         selection = null;
+        redraw = true;
     }
 
     this.remove = function(index) {
         data[index] = "e";
+        redraw = true;
     }
 
     this.highlight = function(cells) {
         highlights = cells;
+        redraw = true;
+    }
+
+    function drawBackground() {
+        ctx.clearRect(0, 0, boardSize, boardSize);
+
+        for (var x = 0; x < BoardDim; x++)
+            for (var y = 0; y < BoardDim; y++) {
+                ctx.fillStyle = highlights.indexOf(indexAt(x, y)) != -1 ? "#afa" : (x + y) % 2 == 0 ? "#eee" : "#ddd";
+                ctx.fillRect(x * cellSize, y * cellSize, cellSize + 1, cellSize + 1);
+            }
+    }
+
+    function drawCircle(x, y, radius, lineWidth, color, fill) {
+        ctx.beginPath();
+        ctx.lineWidth = lineWidth;
+        ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+        ctx.closePath();
+
+        if (fill) {
+            ctx.fillStyle = color;
+            ctx.fill();
+        } else {
+            ctx.strokeStyle = color;
+            ctx.stroke();
+        }
+    }
+
+    function drawPiece(x, y, color, king) {
+        var fill = selection == indexAt(x, y);
+
+        x = (x + 0.5) * cellSize;
+        y = (y + 0.5) * cellSize;
+
+        if (animation != null && fill) {
+            x = x * (1 - animation) + (getX(target) + 0.5) * cellSize * animation;
+            y = y * (1 - animation) + (getY(target) + 0.5) * cellSize * animation;
+        }
+
+        var radius = cellSize / 2 / 1.2;
+
+        drawCircle(x, y, radius, 3, color, fill);
+
+        if (king) {
+            drawCircle(x, y, radius, 3, color);
+            drawCircle(x, y, radius / 1.3, 3, color);
+            drawCircle(x, y, radius / 1.9, 3, color);
+            drawCircle(x, y, radius / 3.5, 3, color);
+        }
+    }
+
+    function processCell(x, y) {
+        if (at(x, y) != "n" && at(x, y) != "e")
+            drawPiece(x, y, (at(x, y) == "x" || at(x, y) == "y") ? "#e33" : "#33e", at(x, y) == "y" || at(x, y) == "p");
+    }
+
+    function drawBoard() {
+        if (data == null)
+            return;
+
+        for (var x = 0; x < BoardDim; x++)
+            for (var y = 0; y < BoardDim; y++)
+                if (selection != indexAt(x, y))
+                    processCell(x, y);
+
+        if (selection != null)
+            processCell(getX(selection), getY(selection));
+
+        if (animation != null) {
+            animation += 0.05;
+
+            if (animation >= 1) {
+                if (getY(target) == 7 && data[selection] == "x")
+                    data[target] = "y";
+                else if (getY(target) == 0 && data[selection] == "o")
+                    data[target] = "p";
+                else
+                    data[target] = data[selection];
+
+                data[selection] = "e";
+
+                selection = null;
+                target = null;
+                animation = null;
+            }
+        }
+    }
+
+    function drawFrame() {
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "#000";
+        ctx.beginPath();
+        ctx.rect(0, 0, boardSize, boardSize);
+        ctx.closePath();
+        ctx.stroke();
     }
 
     this.draw = function() {
-        function getX(index) {
-            return index % BoardDim;
-        }
-
-        function getY(index) {
-            return Math.floor(index / BoardDim);
-        }
-
-        function at(x, y) {
-            return data[indexAt(x, y)];
-        }
-
-        function drawBackground() {
-            ctx.clearRect(0, 0, boardSize, boardSize);
-
-            for (var x = 0; x < BoardDim; x++)
-                for (var y = 0; y < BoardDim; y++) {
-                    ctx.fillStyle = highlights.indexOf(indexAt(x, y)) != -1 ? "#afa" : (x + y) % 2 == 0 ? "#eee" : "#ddd";
-                    ctx.fillRect(x * cellSize, y * cellSize, cellSize + 1, cellSize + 1);
-                }
-        }
-
-        function drawBoard() {
-            if (data == null)
-                return;
-
-            function drawCircle(x, y, radius, lineWidth, color, fill) {
-                ctx.beginPath();
-                ctx.lineWidth = lineWidth;
-                ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-                ctx.closePath();
-
-                if (fill) {
-                    ctx.fillStyle = color;
-                    ctx.fill();
-                } else {
-                    ctx.strokeStyle = color;
-                    ctx.stroke();
-                }
-            }
-
-            function drawPiece(x, y, color, king) {
-                var fill = selection == indexAt(x, y);
-
-                x = (x + 0.5) * cellSize;
-                y = (y + 0.5) * cellSize;
-
-                if (animation != null && fill) {
-                    x = x * (1 - animation) + (getX(target) + 0.5) * cellSize * animation;
-                    y = y * (1 - animation) + (getY(target) + 0.5) * cellSize * animation;
-                }
-
-                var radius = cellSize / 2 / 1.2;
-
-                drawCircle(x, y, radius, 3, color, fill);
-
-                if (king) {
-                    drawCircle(x, y, radius, 3, color);
-                    drawCircle(x, y, radius / 1.3, 3, color);
-                    drawCircle(x, y, radius / 1.9, 3, color);
-                    drawCircle(x, y, radius / 3.5, 3, color);
-                }
-            }
-
-            function processCell(x, y) {
-                if (at(x, y) != "n" && at(x, y) != "e")
-                    drawPiece(x, y, (at(x, y) == "x" || at(x, y) == "y") ? "#e33" : "#33e", at(x, y) == "y" || at(x, y) == "p");
-            }
-
-            for (var x = 0; x < BoardDim; x++)
-                for (var y = 0; y < BoardDim; y++)
-                    if (selection != indexAt(x, y))
-                        processCell(x, y);
-
-            if (selection != null)
-                processCell(getX(selection), getY(selection));
-
-            if (animation != null) {
-                animation += 0.05;
-
-                if (animation >= 1) {
-                    if (getY(target) == 7 && data[selection] == "x")
-                        data[target] = "y";
-                    else if (getY(target) == 0 && data[selection] == "o")
-                        data[target] = "p";
-                    else
-                        data[target] = data[selection];
-
-                    data[selection] = "e";
-
-                    selection = null;
-                    target = null;
-                    animation = null;
-                }
-            }
-        }
-
-        function drawFrame() {
-            ctx.lineWidth = 1;
-            ctx.strokeStyle = "#000";
-            ctx.beginPath();
-            ctx.rect(0, 0, boardSize, boardSize);
-            ctx.closePath();
-            ctx.stroke();
-        }
+        if (!redraw && animation == null)
+            return;
 
         drawBackground();
         drawBoard();
         drawFrame();
+
+        redraw = false;
     }
 }
